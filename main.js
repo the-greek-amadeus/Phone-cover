ec66a1e0fcf9d7a2029ac78be3eb3c2f914e29d5
the-greek-amadeus <58808912+the-greek-amadeus@users.noreply.github.com>
2020-08-10 13:04:32 +0530

    password added

---
+++ main.js
@@ -0,0 +1,10 @@
+function password() {
+    var pass = prompt("please ener the password to view personal files");
+    if (pass == 170320){
+        window.location.replace("/pihu.html");
+    }
+
+  }
+function resueme(){
+    window.location.replace("/reseume.html")
+}
